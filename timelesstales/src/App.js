7223a981bb20d379312070011bbd2b5f8d6bd709
Ivy Ronoh <ronoh.ivy@gmail.com>
2022-08-01 21:54:06 +0300

    initialized Routes

--- timelesstales/src/App.js
+++ timelesstales/src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
 import NavBar from "./components/Navigation/NavBar"
 import Header from "./components/Header/Header"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Login from "./components/Login/Login"
+import Register from "./components/Register/Register"
+import New from "./components/CreateNew/New"
 
 function App() {
   return (
-    <>
+    <Router>
       <NavBar />
-      <Header />
-    </>
+      <Routes>
+        <Route exact path='/' element={<Header/>} />
+        <Route exact path='/login' element={<Login/>} />
+        <Route exact path='/register' element={<Register/>} />
+        <Route exact path='/createNew' element={<New/>} />
+      </Routes>
+    </Router>
   );
 }
 
