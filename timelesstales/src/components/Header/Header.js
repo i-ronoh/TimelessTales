d1e847ebd825b12ab5483dff726ee7dc491fde2a
Ivy Ronoh <ronoh.ivy@gmail.com>
2022-08-01 20:37:11 +0300

    Added bg image to Header component

--- timelesstales/src/components/Header/Header.js
+++ timelesstales/src/components/Header/Header.js
@@ -3,8 +3,11 @@ import "./header.css"
 
 function Header() {
   return (
-    <div>Timeless Tales</div>
+    <div className="headerContainer">
+      <h3 className="title">Timeless Tales</h3>
+      <img src="https://images.unsplash.com/photo-1552660341-7082b02abb1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1630&q=80" alt="Sunset Image" width="100%" height="900px"/>
+    </div>
   )
 }
 
-export default Header
+export default Header;
