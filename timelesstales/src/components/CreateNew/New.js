d538575f07db116abe6925d4af2d332079e2eec1
Ivy Ronoh <ronoh.ivy@gmail.com>
2022-08-01 21:54:49 +0300

    Created nav links to the components

---
+++ timelesstales/src/components/CreateNew/New.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const New = ()=>{
+    return (
+        <div>
+            New Blog
+        </div>
+    )
+}
+
+export default New
--- timelesstales/src/components/Navigation/NavBar.js
+++ timelesstales/src/components/Navigation/NavBar.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import "./navbar.css"
+import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
     <div className="navbar">
       <div className = "navbar-item-left">
         <ul className="nav-menu">
-          <li className="nav-menu-item">HOME</li>
-          <li className="nav-menu-item">ARTICLES</li>
-          <li className="nav-menu-item">CREATE</li>
+          <li className="nav-menu-item" >
+            <Link className="link" to="/">HOME</Link>
+          </li>
+          <li className="nav-menu-item" >
+            <Link className="link"  to="/blogs">BLOG POSTS</Link> 
+          </li>
+          <li className="nav-menu-item">
+            <Link className="link" to="/createNew">NEW POST</Link>
+          </li>
         </ul>
       </div>
       <div className = "navbar-item-center">
@@ -16,9 +23,14 @@ function NavBar() {
         <input type="text" className="nav-search" placeholder="Search"></input>
       </div>
       <div className = "navbar-item-right">
-        <h5 className="nav-menu-item">LOGIN</h5>
-        <h5 className="nav-menu-item">REGISTER</h5>
-
+       <ul className="nav-menu">
+        <li className="nav-menu-item">
+            <Link className="link" to="/login">LOGIN</Link>
+          </li>
+          <li className="nav-menu-item">
+            <Link className="link"  to="/register">REGISTER</Link>
+          </li>
+       </ul>
       </div>
     </div>
   )
