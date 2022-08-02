2b9b35654596f6a01514cb337884c1a8d50b98bb
Ivy Ronoh <ronoh.ivy@gmail.com>
2022-08-02 17:43:47 +0300

    Added page title

--- timelesstales/src/components/CreateNew/New.js
+++ timelesstales/src/components/CreateNew/New.js
@@ -42,9 +42,22 @@ const New = ()=>{
 
 
     return (
+        <>
+        <div className="new-head">
+            <h3 className="new-title">NEW POST</h3>
+        </div>
         <div className="container">
-            <div className="row justify-content-md-center" style={{marginTop: "5em"}}>
-                <div className="col-md-6 shadow p-3 mb-5 bg-body rounded">
+            <div className="row" style={{marginTop: "5em"}}>
+                <div className="col-md-9 shadow p-5 mb-5 bg-body rounded">
+                    <div className="mb-3">
+                        <label htmlFor="exampleFormControlInput1" className="form-label">Author</label>
+                        <input type="title" className="form-control" id="exampleFormControlInput1"
+                            onChange = {(event)=>{
+                                onFieldsChange("author", event.target.value)
+                            }}
+                        />
+    
+                    </div>
                     <div className="mb-3">
                         <label htmlFor="exampleFormControlInput1" className="form-label">Title</label>
                         <input type="title" className="form-control" id="exampleFormControlInput1"
@@ -52,10 +65,11 @@ const New = ()=>{
                                 onFieldsChange("title", event.target.value)
                             }}
                         />
+    
                     </div>
                     <div className="mb-3">
-                        <label htmlFor="exampleFormControlTextarea1" className="form-label">Blog</label>
-                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"
+                        <label htmlFor="exampleFormControlTextarea1" className="form-label">Blog Entry</label>
+                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="5"
                             onChange = {(event)=>{
                                 onFieldsChange("data", event.target.value)
                             }}
@@ -65,12 +79,13 @@ const New = ()=>{
                         <button type="submit" className="btn btn-primary mb-3" disabled={!submitButtonActive}
                             onClick={postBlog}
                         >
-                            Submit
+                            SUBMIT
                         </button>
                     </div>
                 </div>
             </div>
         </div>
+        </>
     )
 }
 
