9578a80cedd0886b938275b827d5f21234234ff5
Ivy Ronoh <ronoh.ivy@gmail.com>
2022-08-03 16:38:24 +0300

    Modified Blog container

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,43 +1,44 @@
-import React, {useEffect, useState} from 'react';
-import "./blog.css"
+import React, { useEffect, useState } from "react";
+import "./blog.css";
 
-function Blogs () {
-    const [blogs, setBlogs] = useState([]);
+function Blogs() {
+  const [blogs, setBlogs] = useState([]);
 
-    useEffect(()=>{
-        fetch('https://cosmic-dark-result.glitch.me/articles')
-        .then((response) => response.json())
-        .then((data) => {
-            setBlogs(data);
-            console.log(data);
-        })
-    }, []);
+  useEffect(() => {
+    fetch("https://cosmic-dark-result.glitch.me/articles")
+      .then((response) => response.json())
+      .then((data) => {
+        setBlogs(data);
+        console.log(data);
+      });
+  }, []);
 
-    return(
-        <div className="blog-container">
-            {
-                blogs.map((blog)=>(
-                    <div className="row" style={{marginBottom: "3em", marginTop: "3em"}} key={blog.id}>
-                        <div className="col-md-5">
-                            <div className="card">
-                                <img src={blog.image} className="card-img-top" alt="..."
-                                    
-                                />
-                                <div className="card-body">
-                                    <h5 className="card-title">
-                                        {blog.title}
-                                    </h5>
-                                    <p className="card-text">
-                                        S{blog.data}
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                ))
-            }
-       </div>
-    )
+  return (
+    <div className="blog-container d-flex flex-wrap">
+      {blogs.map((blog) => (
+        <div
+          className="row "
+          style={{ marginBottom: "3em", marginTop: "3em" }}
+          key={blog.id}
+        >
+          <div className="col-md-5 ">
+            <div className="card flexx" style={{ width:"500px" }}>
+              <img
+                src={blog.image}
+                className="card-img-top img-fluid"
+                alt="..."
+               // style={{ width: "100%" }}
+              />
+              <div className="card-body">
+                <h5 className="card-title">{blog.title}</h5>
+                <p className="card-text">{blog.data}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Blogs;
