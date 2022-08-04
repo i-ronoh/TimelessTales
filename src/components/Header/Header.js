e75821fc922f4acd850439e71bde47adeb2dd3c3
Ivy Ronoh <ronoh.ivy@gmail.com>
2022-08-04 09:05:31 +0300

    Add description

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,6 +7,7 @@ function Header() {
     
     <div className="headerContainer">
       <h3 className="title">Timeless Tales</h3>
+      <p>Read People's Minds, <br/> & Share a Piece of Your Own.</p>
       {/* <img src="https://images.unsplash.com/photo-1552660341-7082b02abb1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1630&q=80" alt="Sunset Image" width="100%" height="900px"/> */}
     </div>
     </>
